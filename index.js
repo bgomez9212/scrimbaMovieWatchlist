8daed046bff8a89b321995d8089b4e3575eb6ac5
Brandon Gomez <105175292+bgomez9212@users.noreply.github.com>
2024-02-23 23:37:44 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+
+const searchBtn = document.getElementById('search-btn')
+const myWatchlistBtn = document.getElementById('my-watchlist-btn')
+let watchListArray = []
+
+searchBtn.addEventListener('click', async function(e){
+    e.preventDefault()
+    document.getElementById('placeholder').style.display = 'none'
+    
+    let searchedFilm = document.getElementById('movie-search').value
+    let filmIdArray = await getFilmIdArray(searchedFilm)
+    
+    if (filmIdArray.length === 0) {
+        document.getElementById('no-results').style.display = 'flex'
+        document.getElementById('results').style.display = 'none'
+    } else {
+        document.getElementById('no-results').style.display = 'none'
+        document.getElementById('results').style.display = 'flex'
+    }
+    
+    document.getElementById('results').innerHTML = await getResultsHtml(filmIdArray)
+})
+
+            
+
+//17073252
+
+// function returns array of IDs returned from user's search input
+async function getFilmIdArray(userInput){
+    const res = await fetch(`https://www.omdbapi.com/?s=${userInput}&apikey=17073252`)
+    const data = await res.json()
+        let filmIdArray = []
+        if (data.Search){
+            for (let film of data.Search){
+                if (film.Type === "movie"){
+                    filmIdArray.push(film.imdbID)
+                }
+            }
+        }
+        return filmIdArray
+}
+
+// function iterates over array of IDs and creates html for each film, renders to browser
+async function getResultsHtml(IdArray){
+    let searchResultHtml = ``
+    for (let filmId of IdArray){
+        const res = await fetch(`https://www.omdbapi.com/?i=${filmId}&apikey=17073252`)
+        const filmData = await res.json()
+            let filmHtml = `
+            <div class="filmData" id="filmData">
+                <img src="${filmData.Poster}">
+                <div class="filmData-desc">
+                    <div class="title">
+                        <h3 class="title">${filmData.Title}</h3>
+                        <p class="rating">${filmData.imdbRating}</p>
+                    </div>
+                    <div class="info">
+                        <p class="runtime">${filmData.Runtime}</p>
+                        <p class="genre">${filmData.Genre}</p>
+                        <button
+                            class="add-to-watchlist" 
+                            id="${filmData.imdbID}"
+                            onclick="addToWatchlist(${filmData.imdbID})">
+                            Watchlist
+                        </button>
+                    </div>
+                    <div class="synopsis">
+                        <p>${filmData.Plot}</p>
+                    </div>
+                </div>
+            </div>`
+        searchResultHtml += filmHtml
+    }
+    return searchResultHtml
+}
+
+function addToWatchlist(element){
+    localStorage.setItem(element.id, element.id)
+}
